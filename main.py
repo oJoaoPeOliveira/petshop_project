08f75fbfbe88bc1257388b95192f08193cf9f9a8
João Pedro <joaopedro_o@outlook.com>
2025-11-02 16:55:42 -0300

    fase inicial

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+from modelos.cliente import Cliente
+
+def main():
+	cliente = Cliente('João Pedro', '00000000', '17/07/1999', '93000000', 'Luffy')
+	cliente = Cliente('João Pedro', '00000001', '17/07/1999', '93000000', 'Barry')
+	Cliente.listar_clientes()
+	
+if __name__ == '__main__':
+	main()
---
+++ modelos/cliente.py
@@ -0,0 +1,26 @@
+class Cliente:
+    
+    clientes = []
+
+    def __init__(self, nome, cpf, telefone, cep):
+        self._nome = nome
+        self._cpf = cpf
+        self._telefone = telefone
+        self._cep = cep
+        
+        self._pets = []
+        self._ativo = True
+    
+    def adicionar_pet(self, animal):
+        self._pets.append(animal)
+    
+    @property
+    def ativo(self):
+        return 'Ativado' if self._ativo else 'Desativado'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+    
+    def __str__(self):
+        numero_pets = len(self._pets)
+        return f'Cliente: {self._nome} CPF: {self._cpf} | Status: {self.ativo} | Pets: {numero_pets}'
